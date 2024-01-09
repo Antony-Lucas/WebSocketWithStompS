2ac3f5436eb28ae44447e291dd03626ebcc708b1
Antony Lucas <54651622+Antony-Lucas@users.noreply.github.com>
2024-01-09 18:03:18 -0300

    stable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+  ViewChild,
+} from '@angular/core';
 import { WebsocketService } from './websocket.service';
+import {
+  Chart,
+  ChartConfiguration,
+  ChartDataset,
+  ChartEvent,
+  ChartType,
+} from 'chart.js';
+import Annotation from 'chartjs-plugin-annotation';
+import { BaseChartDirective } from 'ng2-charts';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +24,158 @@ import { WebsocketService } from './websocket.service';
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  private dataTest: string = '';
-  constructor(private websocketService: WebsocketService) {}
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+  title = 'websockettest';
+  private newLabel = 'New label';
+  public isBrowser: boolean;
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private websocketService: WebsocketService
+  ) {
+    Chart.register(Annotation);
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      },
+    ],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  };
+
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.5,
+      },
+    },
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      y: {
+        position: 'left',
+      },
+      y1: {
+        position: 'right',
+        grid: {
+          color: 'rgba(255,0,0,0.3)',
+        },
+        ticks: {
+          color: 'red',
+        },
+      },
+    },
+
+    plugins: {
+      legend: { display: true },
+      annotation: {
+        annotations: [
+          {
+            type: 'line',
+            scaleID: 'x',
+            value: 'March',
+            borderColor: 'orange',
+            borderWidth: 2,
+            label: {
+              display: true,
+              position: 'center',
+              color: 'orange',
+              content: '',
+              font: {
+                weight: 'bold',
+              },
+            },
+          },
+        ],
+      },
+    },
+  };
+
+  public lineChartType: ChartType = 'line';
 
   ngOnInit(): void {
+    //this.lineChartData.datasets[0].data = [65, 59, 80, 81, 56, 55, 40];
     this.websocketService.dataModel.subscribe((p) => {
-      console.log(p);
+      if (p.teachers) {
+        this.lineChartData.datasets[0].data = p.teachers.datasets[0].data;
+        console.log(p.teachers.datasets[0].data);
+        this.chart?.update();
+      }
     });
+  }
+
+  private static generateNumber(i: number): number {
+    return Math.floor(Math.random() * (i < 2 ? 100 : 1000) + 1);
+  }
+
+  public randomize(): void {
+    for (let i = 0; i < this.lineChartData.datasets.length; i++) {
+      for (let j = 0; j < this.lineChartData.datasets[i].data.length; j++) {
+        this.lineChartData.datasets[i].data[j] = AppComponent.generateNumber(i);
+      }
+    }
+    this.chart?.update();
+  }
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public hideOne(): void {
+    const isHidden = this.chart?.isDatasetHidden(1);
+    this.chart?.hideDataset(1, !isHidden);
+  }
+
+  public pushOne(): void {
+    this.lineChartData.datasets.forEach((x, i) => {
+      const num = AppComponent.generateNumber(i);
+      x.data.push(num);
+    });
+    this.lineChartData?.labels?.push(
+      `Label ${this.lineChartData.labels.length}`
+    );
+
+    this.chart?.update();
+  }
+
+  public changeColor(): void {
+    this.lineChartData.datasets[2].borderColor = 'green';
+    this.lineChartData.datasets[2].backgroundColor = `rgba(0, 255, 0, 0.3)`;
+
+    this.chart?.update();
+  }
+
+  public changeLabel(): void {
+    const tmp = this.newLabel;
+    this.newLabel = this.lineChartData.datasets[2].label;
+    this.lineChartData.datasets[2].label = tmp;
+
+    this.chart?.update();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,22 @@ import {
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { NgChartsConfiguration, NgChartsModule } from 'ng2-charts';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    NgChartsModule,
+    CommonModule,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
